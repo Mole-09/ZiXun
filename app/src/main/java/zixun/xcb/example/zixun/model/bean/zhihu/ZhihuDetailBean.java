84c0ff36b0dadc49b014187af48cfd9fcd323b76
15868802819 <1160417543@qq.com>
2019-04-10 16:42:17 +0800

    first

--- app/src/main/java/zixun/xcb/example/zixun/model/bean/zhihu/ZhihuDetailBean.java
+++ app/src/main/java/zixun/xcb/example/zixun/model/bean/zhihu/ZhihuDetailBean.java
@@ -50,5 +50,84 @@ public class ZhihuDetailBean {
     @SerializedName("css")
     private List<String> css;
 
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body){
+        this.body =body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    public String getShare_url() {
+        return share_url;
+    }
+
+    public void setShare_url(String share_url){
+        this.share_url = share_url;
+    }
+
+    public String getImage_source() {
+        return image_source;
+    }
+
+    public void setImage_source(String image_source){
+        this.image_source = image_source;
+    }
+
+    public String getGa_prefix() {
+        return ga_prefix;
+    }
+
+    public void setGa_prefix(String ga_prefix){
+        this.ga_prefix = ga_prefix;
+    }
+
+    public List<String> getCss() {
+        return css;
+    }
+
+    public void setCss(List<String> css){
+        this.css =css;
+    }
+
+    public List<String> getJs() {
+        return js;
+    }
+
+    public void setJs(List<String> js){
+        this.js =js;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type){
+        this.type =type;
+    }
 
 }
